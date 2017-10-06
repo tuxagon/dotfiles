4f4b80e5f13583221bcea5cf1f49a19148526d3d
Kenneth Bogner <bognerkenny@gmail.com>
2017-10-05 22:32:08 -0400

    Added setup file to install vim bundles

---
+++ vimsetup.sh
@@ -0,0 +1,30 @@
+apt-get install silversearcher-ag
+
+cd ~/.vim/bundle
+
+git clone https://github.com/rking/ag.vim
+git clone https://github.com/w0rp/ale.git
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/ElmCast/elm-vim.git
+git clone https://github.com/eagletmt/ghcmod-vim.git
+git clone https://github.com/eagletmt/neco-ghc.git
+git clone https://github.com/Shougo/neocomplete.vim.git
+git clone https://github.com/scrooloose/nerdcommenter.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/ervandew/supertab.git
+# git clone https://github.com/vim-syntastic/syntastic.git
+git clone https://github.com/godlygeek/tabular.git
+git clone https://github.com/tomtom/tlib_vim.git
+git clone https://github.com/mbbill/undotree.git
+git clone https://github.com/vim-airline/vim-airline.git
+git clone https://github.com/vim-airline/vim-airline-themes.git
+git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+git clone https://github.com/tpope/vim-bundler.git
+git clone https://github.com/easymotion/vim-easymotion.git
+git clone https://github.com/elixir-editors/vim-elixir.git
+git clone https://github.com/tpope/vim-endwise.git
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/vim-ruby/vim-ruby.git
+git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/skalnik/vim-vroom.git
+git clone https://github.com/Shougo/vimproc.vim.git
