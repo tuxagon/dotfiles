26acb717cc340e3f423db07da82b9ce9f37d2ae0
Kenneth Bogner <tuxagon91@gmail.com>
2018-07-26 17:32:58 -0400

    updated dotfiles

--- .zsh/custom/alias.zsh
+++ .zsh/custom/alias.zsh
@@ -18,6 +18,8 @@ alias fvim="vim \$(fzf)"
 alias zshconfig="vim ~/.zshrc"
 alias vimconfig="vim ~/.vimrc"
 
+alias wanip='dig +short myip.opendns.com @resolver1.opendns.com'
+
 # open vscode
 function code {
     if [[ $# = 0 ]]
@@ -28,4 +30,8 @@ function code {
         [[ $1 = /* ]] && argPath="$1" || argPath="$PWD/${1#./}"
         open -a "Visual Studio Code" "$argPath"
     fi
+}
+
+function asdf_postgres_start_966 {
+  pg_ctl -D "$HOME/.asdf/installs/postgres/9.6.6/data/" start
 }
